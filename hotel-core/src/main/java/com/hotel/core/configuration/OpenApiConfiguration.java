43a7e37f1782911880ac704405303467433d1515
pgolpejas <82026087+pgolpejas@users.noreply.github.com>
2024-03-30 22:27:46 +0100

    Add common configuration to library (#13)

--- hotel-core/src/main/java/com/hotel/core/configuration/OpenApiConfiguration.java
+++ hotel-core/src/main/java/com/hotel/core/configuration/OpenApiConfiguration.java
@@ -1,4 +1,4 @@
-package com.reservation.configuration;
+package com.hotel.core.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
--- hotel-core/src/main/java/com/hotel/core/configuration/WebConfiguration.java
+++ hotel-core/src/main/java/com/hotel/core/configuration/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.reservation.configuration;
+package com.hotel.core.configuration;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
