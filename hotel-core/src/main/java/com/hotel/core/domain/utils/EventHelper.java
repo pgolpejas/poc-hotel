18744b5822ba6760e0641d1e5bcac781a10389fe
pgolpejas <82026087+pgolpejas@users.noreply.github.com>
2024-04-29 08:55:34 +0200

    spring cloud event router (#47)

---
+++ hotel-core/src/main/java/com/hotel/core/domain/utils/EventHelper.java
@@ -0,0 +1,13 @@
+package com.hotel.core.domain.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EventHelper {
+
+  public static final String EVENT_DOMAIN_TYPE = "event_domain_type";
+
+  public static final String EVENT_TYPE = "event_type";
+
+}
