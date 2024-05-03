58817b6a5008bb1fbd916f8c7fccc21d595b67e7
pgolpejas <pgolpejas@gmail.com>
2024-05-03 08:17:15 +0200

    Reservation update/delete with availability

--- bc-reservation/src/main/java/com/reservation/application/usecase/DeleteReservationUseCaseImpl.java
+++ bc-reservation/src/main/java/com/reservation/application/usecase/DeleteReservationUseCaseImpl.java
@@ -3,9 +3,11 @@ package com.reservation.application.usecase;
 import com.reservation.domain.exception.ReservationNotFoundException;
 import com.reservation.domain.model.Reservation;
 import com.reservation.domain.repository.ReservationRepository;
+import com.reservation.domain.repository.RoomTypeInventoryRepository;
 import com.reservation.domain.usecase.DeleteReservationUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -15,7 +17,9 @@ import java.util.UUID;
 public class DeleteReservationUseCaseImpl implements DeleteReservationUseCase {
 
     private final ReservationRepository reservationRepository;
+    private final RoomTypeInventoryRepository roomTypeInventoryRepository;
 
+    @Transactional
     @Override
     public void deleteReservation(final UUID id) {
         Optional<Reservation> optionalReservation = reservationRepository.findById(id);
@@ -23,9 +27,17 @@ public class DeleteReservationUseCaseImpl implements DeleteReservationUseCase {
         if (optionalReservation.isEmpty()) {
             throw new ReservationNotFoundException("Reservation with id %s not found", id.toString());
         } else {
-            Reservation aggregate = optionalReservation.get();
-            aggregate.delete();
-            this.reservationRepository.delete(aggregate);
+            Reservation reservation = optionalReservation.get();
+            
+            this.roomTypeInventoryRepository
+                    .findByReservationDates(reservation.hotelId(), reservation.roomTypeId(), reservation.start(), reservation.end())
+                    .forEach(roomTypeInventory -> {
+                        roomTypeInventory.update(roomTypeInventory.totalInventory() + 1, roomTypeInventory.totalReserved() - 1);
+                        this.roomTypeInventoryRepository.save(roomTypeInventory);
+                    });
+
+            reservation.delete();
+            this.reservationRepository.delete(reservation);
         }
     }
 }
